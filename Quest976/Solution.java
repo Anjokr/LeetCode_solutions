5fff076afd6fdac45e1c662a78c526b413478a2f
anjokr <solon7833@gmail.com>
2022-04-07 14:58:57 -0300

    started questionn976 solution

---
+++ Quest976/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        int largest = 0;
+        
+        int side0 = 0;
+        int side1
+        return largest;
+    }
+
+}
---
+++ Quest976/q976.java
@@ -0,0 +1,7 @@
+public class q976 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int [] nums = {2,1,2};
+        System.out.println(sol.largestPerimeter(nums));
+    }
+}
