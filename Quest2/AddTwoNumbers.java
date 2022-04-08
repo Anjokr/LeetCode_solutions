8b92283cab386c603e14e3a8406f5e3eee31486e
Anjokr <solon7833@gmail.com>
2022-04-08 14:28:45 -0300

    quest2 done

--- Quest2/AddTwoNumbers.java
+++ Quest2/AddTwoNumbers.java
@@ -26,6 +26,8 @@ public class AddTwoNumbers {
         node8.next=node9;
         node9.next=node10;
         */
+
+        /*
         ListNode node0 = new ListNode(2);
         ListNode node1 = new ListNode(4);
         ListNode node2 = new ListNode(3);
@@ -39,9 +41,44 @@ public class AddTwoNumbers {
 
         node3.next = node4;
         node4.next = node5;
+        */
+
+        /*
+        ListNode node0 = new ListNode(0);
+        ListNode node1 = new ListNode(0);
+        */
+
+        ListNode node0 = new ListNode(9);
+        ListNode node1 = new ListNode(9);
+        ListNode node2 = new ListNode(9);
+        ListNode node3 = new ListNode(9);
+        ListNode node4 = new ListNode(9);
+        ListNode node5 = new ListNode(9);
+        ListNode node6 = new ListNode(9);
+
+        ListNode node7 = new ListNode(9);
+        ListNode node8 = new ListNode(9);
+        ListNode node9 = new ListNode(9);
+        ListNode node10 = new ListNode(9);
+        
+        node0.next = node1;
+        node1.next=node2;
+        node2.next=node3;
+        node3.next=node4;
+        node4.next=node5;
+        node5.next=node6;
+
+        node7.next=node8;
+        node8.next=node9;
+        node9.next=node10;
 
         Solution sol = new Solution();
-        ListNode result = sol.addTwoNumbers(node0, node3);
+        //ListNode result = sol.addTwoNumbers(node0, node3);
+        ListNode result = sol.addTwoNumbers(node0, node7);
+        while(result.next != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
         System.out.println(result.val);
        
     }
--- Quest2/Solution.java
+++ Quest2/Solution.java
@@ -1,65 +1,38 @@
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int remnant = 0;
-        int l1Size = 0;
-        int l2Size = 0;
-
+        
         // initializing new linkedList to return
-        ListNode nodeCurrent = null;
-        ListNode start = nodeCurrent;
+        ListNode start = new ListNode(0);
+        ListNode nodeCurrent = start;
         ListNode l1Head = l1;
         ListNode l2Head = l2;
+        int remnant = 0;
 
-        while (l1Head.next != null || l2Head.next != null) {
+        while (l1Head != null || l2Head != null) {
+            int unit1 = 0;
+            if(l1Head != null) {
+                unit1 = l1Head.val;
+            }
+
+            int unit2 = 0;
+            if(l2Head != null) {
+                unit2 = l2Head.val;
+            }
+
             // sum of unit value
-            int total = 0;
-            if (l1Head.next == null) {
-                total = 0 + l2Head.val + remnant;
-                //l2Size++;
-                
-                l2Head = l2Head.next;
-            } else if (l2Head.next == null) {
-                total = l1Head.val + 0 + remnant;
-                //l1Size++;
-                
-                l1Head = l1Head.next;
-            } else {
-                total = l1Head.val + l2Head.val + remnant;
-                //l1Size++;
-                //l2Size++;
-                remnant = total/10;
-                l1Head = l1Head.next;
-                l2Head = l2Head.next;
-            }
+            
+            int total = unit1 + unit2 + remnant;
             remnant = total/10;
-            if (nodeCurrent == null) {
-                nodeCurrent = new ListNode(total % 10);
-                start = nodeCurrent;
-                remnant = total / 10;
-            } else {
-                nodeCurrent.next = new ListNode(total % 10);
-                nodeCurrent = nodeCurrent.next;
-            }
-            //go to next node(value)
+            nodeCurrent.next = new ListNode(total % 10);
+            nodeCurrent = nodeCurrent.next;
+            if(l1Head != null) l1Head = l1Head.next;
+            if(l2Head != null) l2Head = l2Head.next; 
 
         }
-
-        if(l1Size > l2Size) {
-            nodeCurrent.next = new ListNode((l1.val + remnant) % 10);
-            nodeCurrent = nodeCurrent.next;
-            remnant = (l1.val + remnant) / 10;
-            nodeCurrent.val = remnant;
-        } else if (l1Size < l2Size) {
-            nodeCurrent.next = new ListNode((l2.val + remnant) % 10);
-            nodeCurrent = nodeCurrent.next;
-            remnant = (l2.val + remnant) / 10;
-            nodeCurrent = new ListNode(remnant);
-        } else {
-            if(remnant != 0) {
-                nodeCurrent.next = new ListNode(remnant);
-            }
+        if(remnant > 0) {
+            nodeCurrent.next = new ListNode(remnant);
         }
-
-        return start;
+        
+        return start.next;
     }
 }
